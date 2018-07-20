e86bd843dd73da9b3456da2bcc01090d20b1e623
Agung Pramono <agung.prasatu@gmail.com>
2018-07-20 09:22:09 +0700

    Register Produk & Reservasi seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call('UserSeeder');
+        $this->call('ReservasiSeeder');
+        $this->call('ProdukSeeder');
     }
 }
