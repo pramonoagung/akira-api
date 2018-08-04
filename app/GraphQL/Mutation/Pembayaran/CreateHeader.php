b5210cf106477b8dcea813251723ba13eba5eeff
ardiankurniawan8 <ardian.smbc@gmail.com>
2018-08-04 12:16:48 +0700

    Update Create Header Transaksi

--- app/GraphQL/Mutation/Pembayaran/CreateHeader.php
+++ app/GraphQL/Mutation/Pembayaran/CreateHeader.php
@@ -8,7 +8,6 @@ use Thunderlabid\Pembayaran\Models\HeaderTransaksi;
 use GraphQL\Type\Definition\ResolveInfo;
 use Thunderlabid\Pembayaran\Models\Pembayaran;
 use Illuminate\Support\Facades\DB;
-use Thunderlabid\Pembayaran\Models\DetailTransaksi;
 use App\Events\CheckVoucherEvent;
 /**
  * User Query
@@ -30,12 +29,7 @@ class CreateHeader extends Mutation
 			'tanggal' => ['name' => 'tanggal', 'type' => Type::string()],
 			'jenis' => ['name' => 'jenis', 'type' => Type::string()],
 			'jumlah' => ['name' => 'jumlah', 'type' => Type::string()],
-			'referensi' => ['name' => 'referensi', 'type' => Type::string()],
-			'ref_id' => ['name' => 'ref_id', 'type' => Type::string()],
-			'produk' => ['name' => 'produk', 'type' => Type::string()],
-			'kuantitas' => ['name' => 'kuantitas', 'type' => Type::string()],
-			'harga' => ['name' => 'harga', 'type' => Type::string()],
-			'diskon' => ['name' => 'diskon', 'type' => Type::string()]
+			'referensi' => ['name' => 'referensi', 'type' => Type::string()]
 		];
 	}
 	public function resolve($root, $args, $context, ResolveInfo $info)
@@ -45,7 +39,6 @@ class CreateHeader extends Mutation
             DB::beginTransaction();
             $header = new HeaderTransaksi;
             $pembayaran = new Pembayaran;
-            $detail = new DetailTransaksi;
             $header->nomor = $args['nomor'];
             $header->tanggal = $args['tanggal'];
             $header->save();
@@ -56,13 +49,6 @@ class CreateHeader extends Mutation
             $pembayaran->id_header_transaksi = $header->id;
             $pembayaran->save();
             
-            $detail->ref_id = $args['ref_id'];
-            $detail->produk = $args['produk'];
-            $detail->kuantitas = $args['kuantitas'];
-            $detail->harga = $args['harga'];
-            $detail->diskon = $args['diskon'];
-            $detail->id_header_transaksi = $header->id;
-            $detail->save();
             DB::Commit();
             return $header;
         }catch(\Exception $e){
