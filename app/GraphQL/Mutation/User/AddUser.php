9753c24794e59681499b5e6fe9feb022c9d3261b
Agung Pramono <agung.prasatu@gmail.com>
2018-09-03 18:40:59 +0700

    Improve register

--- app/GraphQL/Mutation/User/AddUser.php
+++ app/GraphQL/Mutation/User/AddUser.php
@@ -2,47 +2,89 @@
 
 namespace App\GraphQL\Mutation\User;
 
-use GraphQL;
+use GraphQL, DB;
 use GraphQL\Type\Definition\Type;
 use Folklore\GraphQL\Support\Mutation;
 use Thunderlabid\Otorisasi\Models\User;
+use Thunderlabid\Otorisasi\Models\Scope;
+use Thunderlabid\Otorisasi\Models\Tenant;
 
 class AddUser extends Mutation
 {
     protected $attributes = [
         'name' => 'AddUser'
     ];
-
+    
     public function type()
     {
         return GraphQL::type('User');
     }
-
+    
     public function args()
     {
         return [
             'nama'      => ['name' => 'nama',       'type' => Type::string()],
             'username'  => ['name' => 'username',   'type' => Type::string()],
+            'scope'     => ['name' => 'scope',   'type' => Type::string()],
+            'tenant'    => ['name' => 'tenant',   'type' => Type::string()],
             'jk'        => ['name' => 'jk',   'type' => Type::string()],
             'password'  => ['name' => 'password',   'type' => Type::string()]
         ];
     }
-
+    
     public function resolve($root, $args)
     {
         $user   = User::username($args['username'])->first();
-
+        
         if ($user) {
             throw new \Exception("User Exists", 999);
+        }else if(isset($args['scope'])){
+            return $this->createAdmin($args);
+        }else{
+            return $this->createUser($args);
         }
+        
+    }
+    
+    private function createAdmin($args){
+        $args['password']   = 'newadmin';
+        $args['tenant']     = 'AKIRA';
+        try{
+            DB::BeginTransaction();
+            $user   = new User;
+            $user->username      = $args['username'];
+            $user->nama          = $args['nama'];
+            $user->password      = app('hash')->make($args['password']);
+            $user->save();
 
-        $user   = new User;
-        $user->username     = $args['username'];
-        $user->nama         = $args['nama'];
-        $user->jenis_kelamin= $args['jk'];
-        $user->password     = $args['password'];
-        $user->save();
-
-        return $user;
+            $tenant = new Tenant;
+            $tenant->nama        = $args['tenant'];
+            $tenant->save();
+            
+            $scope = new Scope;
+            $scope->scope        = $args['scope'];
+            $scope->user_id      = $user->id;
+            $scope->tenant_id    = $tenant->id;
+            $scope->save();
+            DB::Commit();
+            return $user;
+        }catch(\Exeption $e){
+            DB::Rollback();
+        }
+    }
+    private function createUser($args){
+        try{
+            DB::BeginTransaction();
+            $user   = new User;
+            $user->username     = $args['username'];
+            $user->nama         = $args['nama'];
+            $user->jenis_kelamin= $args['jk'];
+            $user->password     = app('hash')->make($args['password']);
+            $user->save();
+            DB::Commit();
+            return $user;
+        }catch(\Exeption $e){
+            DB::Rollback();
+        }
     }
 }
