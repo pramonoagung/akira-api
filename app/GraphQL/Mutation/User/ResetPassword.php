1f47a6a0b7d0e86afb537371957f3c73812c3de2
Agung Pramono <agung.prasatu@gmail.com>
2018-09-05 01:44:24 +0700

    Code clean up

--- app/GraphQL/Mutation/User/ResetPassword.php
+++ app/GraphQL/Mutation/User/ResetPassword.php
@@ -15,32 +15,31 @@ class ResetPassword extends Mutation
         'name' => 'ResetPassword',
         'description' => 'A mutation'
     ];
-    
+
     public function type()
     {
         return GraphQL::type('User');
     }
-    
+
     public function args()
     {
         return [
-            'username'          => ['name' => 'username',       'type' => Type::nonNull(Type::string())],
-            'password'          => ['name' => 'password',       'type' => Type::nonNull(Type::string())],
-            'new_password'      => ['name' => 'new_password',       'type' => Type::nonNull(Type::string())]
+            'username' => ['name' => 'username', 'type' => Type::nonNull(Type::string())],
+            'password' => ['name' => 'password', 'type' => Type::nonNull(Type::string())],
+            'new_password' => ['name' => 'new_password', 'type' => Type::nonNull(Type::string())]
         ];
     }
-    
+
     public function resolve($root, $args, $context, ResolveInfo $info)
     {
-        $user = User::where('username',$args['username'])->first();
-        if(Hash::check($args['password'], $user->password)){
+        $user = User::where('username', $args['username'])->first();
+        if (Hash::check($args['password'], $user->password)) {
             $user->password = app('hash')->make($args['new_password']);
             $user->save();
-            return $user;    
-        }   
-        else{
+            return $user;
+        } else {
             return response('The Password doesnt match', 500);
-        }     
-        
+        }
+
     }
 }
